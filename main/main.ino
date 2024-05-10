f1dd18298b716fcc6ab8b6e6b91d1a85a4db1afc
Kelly-Shane Fuller <punk.kaos@gmail.com>
2024-05-10 11:14:17 -0700

    Fixes

--- main/main.ino
+++ main/main.ino
@@ -3,9 +3,11 @@ const int potPin = A0;          // Potentiometer pin
 const int motorSpeedPin = 9;    // PWM pin connected to H-bridge input for speed control
 const int motorDirectionPin1 = 8; // Digital pin connected to H-bridge input for direction control 1
 const int motorDirectionPin2 = 7; // Digital pin connected to H-bridge input for direction control 2
+const int fastidle = 6;
 
 void setup() {
   pinMode(potPin, INPUT);            // Set potentiometer pin as input
+  pinMode(fastidle, INPUT);
   pinMode(motorSpeedPin, OUTPUT);    // Set motor speed control pin as output
   pinMode(motorDirectionPin1, OUTPUT); // Set motor direction control pin 1 as output
   pinMode(motorDirectionPin2, OUTPUT); // Set motor direction control pin 2 as output
@@ -26,15 +28,21 @@ void loop() {
 
   // Adjust mapping to focus on the range 600-1023
     
-  int motorSpeed = map(potValue, 150, 700, 100, 255);
+  int motorSpeed = map(potValue, 260, 950, 100, 255);
   //motorSpeed = motorSpeed * motorSpeed / 255;
 
   // Set motor direction based on the potentiometer value
-  if (potValue < 120) {
+  if (potValue < 275) {
     // Set motor direction for reverse
     digitalWrite(motorDirectionPin1, HIGH);
     digitalWrite(motorDirectionPin2, LOW);
-   analogWrite(motorSpeedPin,175);
+    int idle=digitalRead(fastidle);
+    if (idle==LOW) {
+      analogWrite(motorSpeedPin,175);
+    }
+    else{
+    analogWrite(motorSpeedPin,150);
+    }
   }
   else{
     // Set motor direction for forward
